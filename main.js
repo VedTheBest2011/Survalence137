3874142af3ad5b205e79cd15121700dfe3f77a3b
VedTheBest2011 <82335213+VedTheBest2011@users.noreply.github.com>
2022-03-14 19:57:10 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+
+
+video = "";
+status = "";
+objects = [];
+
+function preload()
+{
+video = createVideo('video.mp4');
+video.hide();
+}
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+if(input == objects)
+{
+    videoLiveView.stop();
+    objectDetector.detect(gotResult);
+    document.getElementById("status").innerHTML = "object mentioned found";
+}else{
+    document.getElementById("status").innerHTML = "object mentioned not found";
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+        if(status != "")
+        {
+            objectDetector.detect(video, gotResult);
+
+            for (i = 0; i< objects.length; i++);
+            {
+                document.getElementById("status").innerHTML = "Status : Objects detected";
+                document.getElementById("number_of_objects").innerHTML = "Number of obejcts detected are " + objects.length;
+
+                fill("#FF0000");
+                percent = floor(objects[i].confidence * 100);
+                text(objects[i].label + " " + percent + "%", objects[i].x+ 15, objects[i].y + 15);
+                noFill();
+                stroke("#FF0000");
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            }
+        }
+}
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : detecting objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
